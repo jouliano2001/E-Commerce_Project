8242a9b91fe2ab77d92bf59817363314a4f428d1
jouliano adel <Joulianoadel@gmail.com>
2025-04-29 13:19:28 +0300

    Update

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from "../Components/NavBar/NavBar";
 import AuthLayout from "../Pages/Auth/AuthLayout";
 import Error from "../Pages/Error/Error";
